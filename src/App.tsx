81e849abdeb963f0b7d7c0d4ccd9bca0f676fbe7
Aaron Chen <aaron@Aarons-MacBook-Air.local>
2024-12-03 17:10:26 -0800

    Fix font

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Scene } from './Scene'
 function App() {
   return (
     <div className="w-screen h-screen flex justify-center" style={{ height: "100vh" }}>
-      <h1 className='fixed z-10 mx-auto text-blue-50 mt-20'>Hi Babi! Here's a gift for you!</h1>
+      <h1 className='fixed z-10 mx-auto text-blue-50 mt-20 text-3xl'>Hi Babi! Click to open the gift!</h1>
       <div className='absolute w-full h-full'>
         <Scene />
       </div>
